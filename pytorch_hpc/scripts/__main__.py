1865e33a4537e446213bb9296669673032ad0493
bzfbnick <bzfbnick@bgnlogin2.usr.hlrn.de>
2023-10-30 16:08:29 +0100

    clean up, scripts

--- pytorch_hpc/scripts/__main__.py
+++ pytorch_hpc/scripts/__main__.py
@@ -1,10 +1,8 @@
 import lightning
 from lightning.pytorch.cli import LightningCLI
 
-
 def cli_main():
     cli = LightningCLI(save_config_kwargs={"overwrite": True})
-
 
 if __name__ == "__main__":
     cli_main()
